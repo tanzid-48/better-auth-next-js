9d0dd099ab435708e705a5ae665e3a949a68c8b4
tanzid-48 <mdtanzid.525@gmail.com>
2026-04-23 11:34:07 +0600

    1.Configure BetterAuth with MongoDB adapter  2.Create MongoDB account and MongoDB client  3.Setup auth route, HeroUI and Gravity UI icons

---
+++ src/app/auth/singup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingUpPage = () => {
+    return (
+        <div>
+            <h2>This is Sing up Page </h2>
+        </div>
+    );
+};
+
+export default SingUpPage;
